57f9bde0f31d67e2de8791de37505783cb0eb55f
dqmis <dom.seputis@gmail.com>
2022-10-24 20:51:41 +0300

    Added sql code

--- class_materials/examples/2022-10-24.sql
+++ class_materials/examples/2022-10-24.sql
@@ -18,8 +18,6 @@ CREATE TABLE publishers (
 ALTER TABLE books MODIFY COLUMN publisher_id int NOT NULL;
 ALTER TABLE books ADD CONSTRAINT fk_publisher_id FOREIGN KEY (publisher_id) REFERENCES publishers(id);
 
-ALTER TABLE books DROP FOREIGN KEY fk_publisher_id;
-
 CREATE TABLE genre (
 	id int NOT NULL AUTO_INCREMENT,
 	title varchar(255) NOT NULL,
@@ -52,6 +50,23 @@ CREATE TABLE users (
 	 email varchar(255) NOT NULL UNIQUE,
 	PRIMARY KEY (id)
 )
+
+
+INSERT INTO genres (title) VALUES ("horror");
+
+INSERT INTO genres (title) VALUES ("history"), ("children");
+
+INSERT INTO publishers (title, company_id) VALUES ("sviesa", 12345), ("some publisher", 12245), ("another publisher", 12346);
+
+ALTER TABLE books RENAME COLUMN pulish_date to publish_date;
+
+INSERT INTO books (title, number_of_pages, publish_date, description, publisher_id) VALUES ("frankenstein", 280, "1818-01-01", "Frankenstein tells the story of Victor Frankenstein, a young scientist who creates a sapient creature in an unorthodox scientific experiment", 5)o
+
+INSERT INTO books (title, number_of_pages, publish_date, description, publisher_id) VALUES
+("dracula", 287, "1897-01-01", "„Drakula“ yra epistolinis romanas, parašytas laiškų, dienoraščių, straipsnių, užrašų forma. Grafo Drakulos prototipas yra Valakijos princas Vladas III, Vladas Drakula, pagarsėjęs savo žiaurumu.", 5),
+("it", 1142, "1986-01-01", "Romane „Tas“ aprašoma Deryje gyvenanti ir žudanti vaikus antgamtinė būtybė, gebanti keisti savo esybės formą ir pavidalą", 5),
+("the witcher", 235, "1990-01-01", "The series revolves around the eponymous witcher, Geralt of Rivia. In Sapkowski's works, witchers are beast hunters who develop supernatural abilities at a young age to battle wild beasts and monsters", 4),
+("pride and prejudice", 654, "1813-01-01", "The novel follows the character development of Elizabeth Bennet, the dynamic protagonist of the book who learns about the repercussions of hasty judgments and comes to appreciate the difference between superficial goodness and actual goodness.", 6);
 
 
 
